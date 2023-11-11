de4c2cc8d78b77be2d5de7d49ada2e5cded60dc4
Prosper Atu <atu.prosper@gmail.com>
2023-11-11 15:40:24 -0800

    created the strlen function

--- main.h
+++ main.h
@@ -1,7 +1,24 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int _putchar(char character);
+/**
+ * struct format_struct - This struct contains information on the format.
+ * It defines two members which will be used in our program
+ * @specifier: Character specified by the user to check for
+ * @func_ptr: Pointer to function to
+ */
+typedef struct format_struct
+{
+	char *specifier;
+	void (*func_ptr)();
+} prt_fmt_t;
+
+int _strlen(char *val);
 int _printf(const char *format, ...);
 
 #endif
+
