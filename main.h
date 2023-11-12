a5b1f9535a8199902eb633a8ef2e54c590483220
Prosper Atu <atu.prosper@gmail.com>
2023-11-11 19:02:25 -0800

    removed struct from header file

--- main.h
+++ main.h
@@ -6,18 +6,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-/**
- * struct formats - This struct contains information on the format.
- * It defines two members which will be used in our program
- * @specifier: Character specified by the user to check for
- * @func_ptr: Pointer to function to
- */
-typedef struct formats
-{
-	char *specifier;
-	void (*func_ptr)();
-} prt_fmt_t;
-
 int _printf(const char *format, ...);
 
 #endif
